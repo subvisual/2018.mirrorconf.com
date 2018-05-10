e160c64c490e2a619938e60126c2d3dcb264dd20
jfsgomes <jfsg91@gmail.com>
2018-05-10 01:55:19 +0100

    Update Navigation and About
    This change addresses the issue by:
    
    * Updating the copy of the About section;
    * Adding images to the About section;
    * Adding contrasts to Navigation;
    * Fixing positioning of elements in Navigation;

--- src/components/About/Viewport/index.js
+++ src/components/About/Viewport/index.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 
+import Attendee from './attendee.png';
 import Bullet from './dot.svg';
+import Quizz from './quizz.png';
+import Venue from './venue.png';
+import Speaker from './speaker.png';
 
 import './index.module.css';
+
+const SOCIAL_LINKS = [
+  {
+    name: 'facebook',
+    link: 'https://www.facebook.com/mirrorconf/',
+  },
+  {
+    name: 'twitter',
+    link: 'https://twitter.com/mirrorconf',
+  },
+  {
+    name: 'youtube',
+    link: 'https://www.youtube.com/channel/UCDez53TT1_v3jr3lGv-QhKw',
+  }
+];
 
 const EVENT_STATS = [
   {
@@ -43,6 +62,17 @@ export default class CommandBar extends Component {
     );
   };
 
+  renderSocialLinks = (socialNetwork, index) => {
+    return (
+      <li role="listitem" key={index}>
+        <img aria-hidden="true" src={Bullet} styleName="bullet" />
+        <p styleName="bulletpoint">
+          {socialNetwork.name} - <a href={socialNetwork.link}>{socialNetwork.link}</a>
+        </p>
+      </li>
+    );
+  };
+
   render() {
     return (
       <div ref={this.props.onViewport} styleName="root">
@@ -56,6 +86,13 @@ export default class CommandBar extends Component {
           collaborative future.
         </p>
         <div styleName="division" />
+        <h3 id="social-links" styleName="visuallyHidden">
+          Social Links
+        </h3>
+        <ul aria-labelledby="social-links" styleName="text" role="list">
+          {SOCIAL_LINKS.map(this.renderSocialLinks)}
+        </ul>
+        <div styleName="division" />
         <h2 styleName="subtitle">2018 - The future of web</h2>
         <div styleName="division" />
         <h3 id="event-details" styleName="visuallyHidden">
@@ -65,6 +102,11 @@ export default class CommandBar extends Component {
           {EVENT_STATS.map(this.renderList)}
         </ul>
         <div styleName="division" />
+        <img aria-hidden="true" src={Attendee} styleName="verticalImage" />
+        <img aria-hidden="true" src={Speaker} styleName="horizontalImage" />
+        <img aria-hidden="true" src={Quizz} styleName="horizontalImage" />
+        <img aria-hidden="true" src={Venue} styleName="verticalImage" />
+
       </div>
     );
   }
