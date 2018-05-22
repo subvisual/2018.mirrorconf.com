abc80a666b8933f115ce06320b6682df34de85e8
João Justo <jpjustonunes@gmail.com>
2018-05-22 16:55:26 +0100

    Add location section

---
+++ src/pages/location.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import Location from '../components/Location/index.jsx';
+
+export default class LocationPage extends Component {
+  render() {
+    return <Location />;
+  }
+}
