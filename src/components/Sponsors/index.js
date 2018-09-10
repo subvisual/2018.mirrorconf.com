59b2681419f85959ced986565092d7d5d9f58ea9
jfsgomes <jfsg91@gmail.com>
2018-09-10 15:53:45 +0100

    Add sponsors

--- src/components/Sponsors/index.js
+++ src/components/Sponsors/index.js
@@ -4,62 +4,56 @@ import './index.css';
 
 import wifiLabel from './wifi_label.svg';
 import quizLabel from './quiz_label.svg';
+import partnershipsLabel from './partnerships_label.svg';
 import stoneLabel from './stone_label.svg';
 import mediaLabel from './media_label.svg';
 import goldLabel from './gold_label.svg';
 import platinumLabel from './platinum_label.svg';
 
-import SeegnoLogo from './seegno.svg';
+import BalsamiqLogo from './logos/balsamiq.png';
+import BurocratikLogo from './logos/burocratik.svg';
+import HostelworldLogo from './logos/hostelworld.svg';
+import MediawebLogo from './logos/mediaweb.png';
+import PixelmattersLogo from './logos/pixelmatters.svg';
+import PWITLogo from './logos/portuguese_women_in_tech.png';
+
 
 export default class Sponsors extends Component {
   render() {
     return (
       <section className="Sponsors" id="sponsors">
         <div className="Sponsors-content">
-          <div className="Sponsors-group">
-            <img alt="Wifi sponsor" src={wifiLabel} />
-            <div className="Sponsors-groupContent">
-              <img alt="Seegno" className="Sponsors-logo" src={SeegnoLogo} />
+          <div className="Sponsors-topGroup">
+            <div className="Sponsors-group">
+              <img alt="Platinum sponsors" src={platinumLabel} />
+              <div className="Sponsors-groupContent">
+              </div>
             </div>
-          </div>
-          <div className="Sponsors-group">
-            <img alt="Quiz sponsor" src={quizLabel} />
-            <div className="Sponsors-groupContent">
-              <img alt="Seegno" className="Sponsors-logo" src={SeegnoLogo} />
-              <img alt="Seegno" className="Sponsors-logo" src={SeegnoLogo} />
-              <img alt="Seegno" className="Sponsors-logo" src={SeegnoLogo} />
+            <div className="Sponsors-group">
+              <img alt="Gold sponsors" src={goldLabel} />
+              <div className="Sponsors-groupContent">
+                <img alt="Hostelworld" className="Sponsors-logo" src={HostelworldLogo} />
+              </div>
             </div>
           </div>
           <div className="Sponsors-group">
             <img alt="Stone sponsors" src={stoneLabel} />
             <div className="Sponsors-groupContent">
-              <img alt="Seegno" className="Sponsors-logo" src={SeegnoLogo} />
-              <img alt="Seegno" className="Sponsors-logo" src={SeegnoLogo} />
-              <img alt="Seegno" className="Sponsors-logo" src={SeegnoLogo} />
+              <img alt="Balsamiq" className="Sponsors-logo" src={BalsamiqLogo} />
+              <img alt="Pixelmatters" className="Sponsors-logo" src={PixelmattersLogo} />
+              <img alt="Mediaweb" className="Sponsors-logo" src={MediawebLogo} />
+              <img alt="Burocratik" className="Sponsors-logo" src={BurocratikLogo} />
+            </div>
+          </div>
+          <div className="Sponsors-group">
+            <img alt="Partnerships" src={partnershipsLabel} />
+            <div className="Sponsors-groupContent">
+              <img alt="Portuguese Women In Tech" className="Sponsors-logo" src={PWITLogo} />
             </div>
           </div>
           <div className="Sponsors-group">
             <img alt="Media sponsors" src={mediaLabel} />
             <div className="Sponsors-groupContent">
-              <img alt="Seegno" className="Sponsors-logo" src={SeegnoLogo} />
-              <img alt="Seegno" className="Sponsors-logo" src={SeegnoLogo} />
-              <img alt="Seegno" className="Sponsors-logo" src={SeegnoLogo} />
-            </div>
-          </div>
-          <div className="Sponsors-group">
-            <img alt="Gold sponsors" src={goldLabel} />
-            <div className="Sponsors-groupContent">
-              <img alt="Seegno" className="Sponsors-logo" src={SeegnoLogo} />
-              <img alt="Seegno" className="Sponsors-logo" src={SeegnoLogo} />
-              <img alt="Seegno" className="Sponsors-logo" src={SeegnoLogo} />
-            </div>
-          </div>
-          <div className="Sponsors-group">
-            <img alt="Platinum sponsors" src={platinumLabel} />
-            <div className="Sponsors-groupContent">
-              <img alt="Seegno" className="Sponsors-logo" src={SeegnoLogo} />
-              <img alt="Seegno" className="Sponsors-logo" src={SeegnoLogo} />
-              <img alt="Seegno" className="Sponsors-logo" src={SeegnoLogo} />
             </div>
           </div>
           <div className="Sponsors-group">
