2472ddef46956895b33e5bf65a7a88ef63948778
jfsgomes <jfsg91@gmail.com>
2018-10-12 19:26:23 +0100

    Update sponsors section

--- src/components/Sponsors/index.js
+++ src/components/Sponsors/index.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 
 import './index.css';
 
-import wifiLabel from './wifi_label.svg';
-import quizLabel from './quiz_label.svg';
 import partnershipsLabel from './partnerships_label.svg';
 import stoneLabel from './stone_label.svg';
-import mediaLabel from './media_label.svg';
 import goldLabel from './gold_label.svg';
 import platinumLabel from './platinum_label.svg';
+import thanks from './thanks.svg';
 
 import BalsamiqLogo from './logos/balsamiq.png';
 import BoschLogo from './logos/bosch.svg';
@@ -26,6 +24,7 @@ import ProzisLogo from './logos/prozis.svg';
 import SeegnoLogo from './logos/seegno.svg';
 import StartupBragaLogo from './logos/startup@2x.png';
 import TndsLogo from './logos/tnds.svg';
+import UtrustLogo from './logos/utrust.svg';
 import XingLogo from './logos/xing.svg';
 
 export default class Sponsors extends Component {
@@ -33,39 +32,41 @@ export default class Sponsors extends Component {
     return (
       <section className="Sponsors" id="sponsors">
         <div className="Sponsors-content">
-          <div className="Sponsors-topGroup">
-            <div className="Sponsors-group">
-              <img alt="Platinum sponsors" src={platinumLabel} />
-              <div className="Sponsors-groupContent">
-              </div>
-            </div>
-            <div className="Sponsors-group">
-              <img alt="Gold sponsors" src={goldLabel} />
-              <div className="Sponsors-groupContent">
-                <a href="http://www.farfetch.com/">
-                  <img alt="Farfetch" className="Sponsors-logo" src={FarfetchLogo} />
-                </a>
-                <a href="https://www.hostelworld.com/">
-                  <img alt="Hostelworld" className="Sponsors-logo" src={HostelworldLogo} />
-                </a>
-                <a href="https://www.xing.com/en">
-                  <img alt="Xing" className="Sponsors-logo" src={XingLogo} />
-                </a>
-                <a href="https://www.hi-interactive.pt/en/">
-                  <img alt="Hi-Interactive" className="Sponsors-logo" src={HiLogo} />
-                </a>
-                <a href="https://www.hi-interactive.pt/en/">
-                  <img alt="SeegnoLogo" className="Sponsors-logo" src={SeegnoLogo} />
-                </a>
-                <a href="https://ginetta.net/">
-                  <img alt="Ginetta" className="Sponsors-logo" src={GinettaLogo} />
-                </a>
-                <a href="https://www.outsystems.com/">
-                  <img alt="Outsystems" className="Sponsors-logo" src={OutsystemsLogo} />
-                </a>
-              </div>
+          <div className="Sponsors-group">
+            <img alt="Platinum sponsors" src={platinumLabel} />
+            <div className="Sponsors-groupContent">
+              <a href="http://www.utrust.com/">
+                <img alt="Utrust" className="Sponsors-pwit" src={UtrustLogo} />
+              </a>
             </div>
           </div>
+          <div className="Sponsors-group">
+            <img alt="Gold sponsors" src={goldLabel} />
+            <div className="Sponsors-groupContent">
+              <a href="http://www.farfetch.com/">
+                <img alt="Farfetch" className="Sponsors-logo" src={FarfetchLogo} />
+              </a>
+              <a href="https://www.hostelworld.com/">
+                <img alt="Hostelworld" className="Sponsors-logo" src={HostelworldLogo} />
+              </a>
+              <a href="https://www.xing.com/en">
+                <img alt="Xing" className="Sponsors-logo" src={XingLogo} />
+              </a>
+              <a href="https://www.hi-interactive.pt/en/">
+                <img alt="Hi-Interactive" className="Sponsors-logo" src={HiLogo} />
+              </a>
+              <a href="https://www.hi-interactive.pt/en/">
+                <img alt="SeegnoLogo" className="Sponsors-logo" src={SeegnoLogo} />
+              </a>
+              <a href="https://ginetta.net/">
+                <img alt="Ginetta" className="Sponsors-logo" src={GinettaLogo} />
+              </a>
+              <a href="https://www.outsystems.com/">
+                <img alt="Outsystems" className="Sponsors-logo" src={OutsystemsLogo} />
+              </a>
+            </div>
+          </div>
+
           <div className="Sponsors-group">
             <img alt="Stone sponsors" src={stoneLabel} />
             <div className="Sponsors-groupContent">
@@ -107,13 +108,9 @@ export default class Sponsors extends Component {
             </div>
           </div>
           <div className="Sponsors-group">
-            <img alt="Media sponsors" src={mediaLabel} />
-            <div className="Sponsors-groupContent">
-            </div>
+            <img alt="Thank you!" src={thanks} />
           </div>
-          <div className="Sponsors-group">
-            <div className="Sponsors-firePlaholder" />
-          </div>
+          <div className="Sponsors-firePlaholder" />
         </div>
       </section>
     );
